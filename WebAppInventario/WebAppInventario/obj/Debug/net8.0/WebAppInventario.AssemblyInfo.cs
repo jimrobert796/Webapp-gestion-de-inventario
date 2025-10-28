e3047efe105fcf25ab177d7c481cf86df3cfe24d
jimrobert796 <jimmyroberto270@gmail.com>
2025-10-28 14:26:24 -0600

    Arreglado bug de registro de producto sin fechas y agregado la base de los demas .html

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6bb43b9eae54aaf51027064f25a31bb4e7ff4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0e2f986400849b739b2bdfadf50d627b87d4a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
