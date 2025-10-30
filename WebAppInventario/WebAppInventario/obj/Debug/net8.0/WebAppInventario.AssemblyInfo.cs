8cf0a1bcf948a916969c3d3c80bfe5aa36b898b4
jimrobert796 <jimmyroberto270@gmail.com>
2025-10-29 23:49:12 -0600

    Ventanas mejoradas con modal -> para crud y las demas ventanas mejor vistas

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0def3fbf3a0ba32db03d9cab3cc749792d1754da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a831778feb58cb61917a61676b742851b7322f7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
