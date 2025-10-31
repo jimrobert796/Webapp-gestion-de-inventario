d82ae26c15a51cdd065c4cc2232430d45dba439b
jimrobert796 <jimmyroberto270@gmail.com>
2025-10-31 14:45:02 -0600

    Modales para los 4 cruds, con iconos y mejor ordenados

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf0a1bcf948a916969c3d3c80bfe5aa36b898b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32aed1ab549656d89c271c823c0ecf8e543ec5e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
