bf7bfbe92aa5e89d20bcab867dfcef412d8a43fc
jimrobert796 <jimmyroberto270@gmail.com>
2025-11-02 13:55:53 -0600

    index html actualizado visualmente para luego aplicar el login

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d608e93f6c3d31fb5882dd72a7108517264e984b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44fc2065a643976a8e371489afc795c606f54d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
