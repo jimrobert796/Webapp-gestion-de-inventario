5076b97e26397a160c1d48f04c5d78e3970162fc
zac871 <josueortezgaitan@gmail.com>
2025-11-09 10:43:15 -0600

    version prueba (1.0)

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b057f078c60b3f7218c74ba03ff4e639773a40c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c64707ee158853a2a19be04e41ea4ca37ad36d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
