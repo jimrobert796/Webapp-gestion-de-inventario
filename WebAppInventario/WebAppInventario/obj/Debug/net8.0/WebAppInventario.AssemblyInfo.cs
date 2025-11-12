5cbe954af14dce66ac9bdc8e9271e39c8d15c0fc
zac871 <josueortezgaitan@gmail.com>
2025-11-11 18:05:26 -0600

    implementacion de el Json y animacion en el login interactivo (falta pulir el tamaño y que si responda bien a la escritura de el usuario)

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16574b13bd09b15c7bac572ad598eaaa0be4a194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4291801deb847e8096b2c7fe582ce462479d706")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
