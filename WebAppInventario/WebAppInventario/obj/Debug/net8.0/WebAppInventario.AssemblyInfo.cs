35146ee9538d88f12ec2930ebc15244193b33d14
zac871 <josueortezgaitan@gmail.com>
2025-11-13 13:27:49 -0600

    icono en la pestaña del navegador (uso de  favicon), cambio de tipografia (importadas de google fonts = monserrat, inter)  cambio de estilos en el apartado de productos.html

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b36ade15ffb02c7d3060ed05a5af1c8714766d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed77b4667871c55ed499c987f9c72d75389e8be")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
