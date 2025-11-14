23c81bb9fbcafdc9b0bdb1d3af78c74f664f8cf9
zac871 <josueortezgaitan@gmail.com>
2025-11-13 22:37:09 -0600

    se agrego estilos a categorias.html

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35146ee9538d88f12ec2930ebc15244193b33d14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68bb19d92d8d1522f85fc8fb8c495314f9c3d821")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
