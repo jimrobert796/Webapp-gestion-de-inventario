b8bf3f2f13dbe38b7d32dc562c7c6b10f1380935
jimrobert796 <jimmyroberto270@gmail.com>
2025-11-14 11:56:33 -0600

    dashboard mejorado e inicio del modal para dashboard

---
+++ WebAppInventario/WebAppInventario/Models/Dashboard.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAppInventario.Models
+{
+    public class Dashboard
+    {
+        public int ProductosTotales { get; set; }
+        public decimal VentasHoy { get; set; }
+        public decimal ComprasMes { get; set; }
+        public int Clientes { get; set; }
+        public int Proveedores { get; set; }
+        public int DevolucionesMes { get; set; }
+        public int StockBajo { get; set; }
+        public int FacturasHoy { get; set; }
+        public decimal VentasTotalesMes { get; set; }
+        public decimal PromedioDiario { get; set; }
+    }
+}
