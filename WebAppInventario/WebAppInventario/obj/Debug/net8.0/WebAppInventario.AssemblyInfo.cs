181f49ecf1e10a0ecf3136972b208c64e20dd08c
jimrobert796 <jimmyroberto270@gmail.com>
2025-11-18 21:36:20 -0600

    Correciones en textos de ver detalles

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1c2ea22a842e5ae569361fe2b1423b57f6e351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7699aafcf6ac00c8948fc08bb1170e3541f3eaff")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
