4e1c2ea22a842e5ae569361fe2b1423b57f6e351
jimrobert796 <jimmyroberto270@gmail.com>
2025-11-18 18:24:05 -0600

    Integracion de db_inventario_script para tener la bd lista para hacer las tablas requerido hacer la db_inventario y retoque menor en compras html

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4395199986c651fdbea132a851c0e267c67af63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10548e627beb66880c0bac3293468a8e34f0b82b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
