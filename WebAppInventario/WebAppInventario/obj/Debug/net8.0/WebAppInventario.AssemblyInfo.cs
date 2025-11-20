cc645c9b887857168508e5fd4f5948b1110f4c8c
zac871 <josueortezgaitan@gmail.com>
2025-11-20 07:22:19 -0600

    correccion en los cruds de de agregar, eliminar, modificar (letras y iconos)

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c81bb9fbcafdc9b0bdb1d3af78c74f664f8cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4485dd920ea125e3a0344f471933905b01da8b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
