e4b19415ca54ce779040811bb424ca1122b3fa71
zac871 <josueortezgaitan@gmail.com>
2025-11-20 09:22:20 -0600

    agregado del easter en el index probabilidad 90%

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78e42e1e13db318ff17d5756d0f2e4423f5e4a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12f8b702f82971fbccba9b83cc0c17d1450bcad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
