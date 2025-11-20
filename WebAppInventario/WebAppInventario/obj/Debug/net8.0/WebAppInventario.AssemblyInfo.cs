12f8b702f82971fbccba9b83cc0c17d1450bcad2
zac871 <josueortezgaitan@gmail.com>
2025-11-20 08:37:15 -0600

    cambio en los modales para los mensajes de exito, eliminar y error en todos los cruds de edicion

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc645c9b887857168508e5fd4f5948b1110f4c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78e42e1e13db318ff17d5756d0f2e4423f5e4a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
