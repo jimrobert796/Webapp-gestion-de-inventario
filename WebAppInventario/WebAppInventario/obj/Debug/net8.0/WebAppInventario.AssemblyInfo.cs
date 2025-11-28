bb8ed7202f6fd5c6d635d364727581da2221724d
jimrobert796 <jimmyroberto270@gmail.com>
2025-11-28 15:25:01 -0600

    Se arreglaron las animaciones de busqueda a cada uno de los apartados/tablas y tambien cuando no se encuentra con los filtros de busqueda

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d425717230ebf0ac625f29585db941061490b11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79ac6f3d960cbcf167affdf2a65b3a8d4209b3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
