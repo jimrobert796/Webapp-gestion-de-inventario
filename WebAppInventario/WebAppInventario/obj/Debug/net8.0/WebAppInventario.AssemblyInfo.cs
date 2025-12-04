d9f7002542728d635a6fdfbde5efcb10417d0329
jimrobert796 <jimmyroberto270@gmail.com>
2025-12-04 15:02:23 -0600

    Cambios minusiosos de pulimeinto en la mayoria de los html, falta modificar el readme y la licencia

--- WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
+++ WebAppInventario/WebAppInventario/obj/Debug/net8.0/WebAppInventario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8ed7202f6fd5c6d635d364727581da2221724d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6e6a77022d31761063dd5c9dd668074b495e88")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
